46c04e653cab59ff8ce6fa4c00dbbf51e24347de
leafarpontes <rafapontes2008@hotmail.com>
2022-08-16 23:55:49 -0300

    added a map to iterate the tasks array

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React from "react";
 
 export const List = () => {
+  const tasks = [
+    {
+      task: "React",
+      time: "02:00:00",
+    },
+    {
+      task: "JavaScript",
+      time: "01:00:00",
+    },
+    {
+      task: "TypeScript",
+      time: "03:00:00",
+    },
+  ];
+
   return (
     <aside>
       <h2>Day studies</h2>
       <ul>
-        <li>
-          <h3>React</h3>
-          <span>02:00:00</span>
-        </li>
-        <li>
-          <h3>JavaScript</h3>
-          <span>01:00:00</span>
-        </li>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <h3>{task.task}</h3>
+            <span>{task.time}</span>
+          </li>
+        ))}
       </ul>
     </aside>
-  )
-}
+  );
+};
